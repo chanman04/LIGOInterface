f747b1cbc35cc01517f40f035ce77f98b0c8df99
Danny Truong <dannyalmighty@icloud.com>
2016-02-11 10:19:39 -0600

    finished gravitational theory panel

--- LIGO/src/ligo/Main_Menu.java
+++ LIGO/src/ligo/Main_Menu.java
@@ -33,8 +33,9 @@ public class Main_Menu extends javax.swing.JFrame {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jButton1.setBackground(new java.awt.Color(141, 37, 175));
+        jButton1.setBackground(new java.awt.Color(255, 255, 255));
         jButton1.setFont(new java.awt.Font("Trebuchet MS", 0, 30)); // NOI18N
+        jButton1.setIcon(new javax.swing.ImageIcon("/Users/danny/LIGOInterface/LIGO/images/draw_lg.jpg")); // NOI18N
         jButton1.setText("LIGO");
         jButton1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -44,16 +45,19 @@ public class Main_Menu extends javax.swing.JFrame {
 
         jButton2.setBackground(new java.awt.Color(141, 37, 175));
         jButton2.setFont(new java.awt.Font("Trebuchet MS", 0, 30)); // NOI18N
-        jButton2.setText("LIGO");
+        jButton2.setIcon(new javax.swing.ImageIcon("/Users/danny/LIGOInterface/LIGO/images/interstellar-black-hole.jpg")); // NOI18N
+        jButton2.setText("Black Holes");
         jButton2.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton2ActionPerformed(evt);
             }
         });
 
-        jButton3.setBackground(new java.awt.Color(141, 37, 175));
+        jButton3.setBackground(new java.awt.Color(51, 51, 51));
         jButton3.setFont(new java.awt.Font("Trebuchet MS", 0, 30)); // NOI18N
-        jButton3.setText("LIGO");
+        jButton3.setForeground(new java.awt.Color(255, 255, 255));
+        jButton3.setIcon(new javax.swing.ImageIcon("/Users/danny/LIGOInterface/LIGO/images/Einstein gravity.jpg")); // NOI18N
+        jButton3.setText("Gravitational theory");
         jButton3.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton3ActionPerformed(evt);
@@ -62,7 +66,8 @@ public class Main_Menu extends javax.swing.JFrame {
 
         jButton4.setBackground(new java.awt.Color(141, 37, 175));
         jButton4.setFont(new java.awt.Font("Trebuchet MS", 0, 30)); // NOI18N
-        jButton4.setText("LIGO");
+        jButton4.setIcon(new javax.swing.ImageIcon("/Users/danny/LIGOInterface/LIGO/images/figure1.0.0.jpg")); // NOI18N
+        jButton4.setText("Questions and Answers");
         jButton4.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton4ActionPerformed(evt);
@@ -75,15 +80,13 @@ public class Main_Menu extends javax.swing.JFrame {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 800, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 800, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(0, 0, Short.MAX_VALUE)))
-                .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(jButton4, javax.swing.GroupLayout.PREFERRED_SIZE, 800, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 800, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jButton3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jButton4, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)
+                    .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 959, Short.MAX_VALUE))
                 .addContainerGap())
         );
         layout.setVerticalGroup(
@@ -91,13 +94,12 @@ public class Main_Menu extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 400, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 400, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, 453, Short.MAX_VALUE)
+                    .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE))
                 .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 400, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jButton4, javax.swing.GroupLayout.PREFERRED_SIZE, 400, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap())
+                    .addComponent(jButton3, javax.swing.GroupLayout.DEFAULT_SIZE, 491, Short.MAX_VALUE)
+                    .addComponent(jButton4, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)))
         );
 
         pack();
@@ -112,12 +114,12 @@ public class Main_Menu extends javax.swing.JFrame {
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
         // jPanel1.setBackground(Color.BLUE);
         new Main_Menu().dispose();
-        new Panel2().setVisible(true);
+        new Panel3().setVisible(true);
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
         new Main_Menu().dispose();
-        new Panel3().setVisible(true);
+        new Panel2().setVisible(true);
     }//GEN-LAST:event_jButton3ActionPerformed
 
     private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
--- LIGO/src/ligo/Panel2.java
+++ LIGO/src/ligo/Panel2.java
@@ -33,6 +33,9 @@ public class Panel2 extends javax.swing.JFrame {
     private void initComponents() {
 
         jButton1 = new javax.swing.JButton();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTextArea1 = new javax.swing.JTextArea();
+        jLabel1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setBackground(new java.awt.Color(102, 102, 255));
@@ -44,21 +47,39 @@ public class Panel2 extends javax.swing.JFrame {
             }
         });
 
+        jTextArea1.setBackground(new java.awt.Color(51, 51, 51));
+        jTextArea1.setColumns(20);
+        jTextArea1.setFont(new java.awt.Font("Dialog", 0, 18)); // NOI18N
+        jTextArea1.setForeground(new java.awt.Color(255, 255, 255));
+        jTextArea1.setRows(5);
+        jTextArea1.setText("          According to newtonian theory, gravitational effects propagate from place to place instantaneously. With the\n advent of Einstein's special theory of relativity in 1905, a theory uniting the concepts of space and time into that\n of four dimensional flat space-time (named Minkowski space-time after the mathematician Hermann Minkowski),\n a problem became discernible with newtonian theory. According to special relativity, which is the current guideline\n to the form of all physical theory, the speed of light, c = 3 x 1010 cm s-1, is the top speed allowed to physical\n particles or forces: There can be no instantaneous propagation. After a decade of search for new concepts to \nmake gravitational theory compatible with the spirit of special relativity, Einstein came up with the theory of general\n relativity (1915), the prototype of all modern gravitational theories. Its crucial ingredient, involving a colossal\n intellectual jump, is the concept of gravitation, not as a force, but as a manifestation of the curvature of\n space-time, an idea first mentioned in rudimentary form by the mathematician Ceorg Bernhard Riemann in 1854.\n In Einstein's hands gravitation theory was thus transformed from a theory of forces into the first dynamical theory\n of geometry, the geometry of four dimensional curved space-time.\n          Why talk of curvature? One of Einstein's first predictions was the gravitational redshift: As any wave, such as light,\n propagates away from a gravitating mass, all frequencies in it are reduced by an amount proportional to the\n change in gravitational potential experienced by the wave. This redshift has been measured in the laboratory, in\n solar observations, and by means of high precision clocks flown in airplanes. However, imagine for a moment\n that general relativity had not yet been invented, but the redshift has already been measured. According to a\n simple argument owing to Alfred Schild, wave propagation under stationary circumstances can display a redshift\n only if the usual geometric relations implicit in Minkowski space-time are violated: The space-time must be curved.\n The observations of the redshift thus show that space-time must be curved in the vicinity of\n masses, regardless of the precise form of the gravitational theory.\n          Einstein provided 10 equations relating the metric (a tensor with 10 independent components describing the\n geometry of space-time) to the material energy momentum tensor (also composed of 10 components, one of\n which corresponds to our previous rho). These Einstein field equations, in which both of the previously mentioned\n constants G and c figure as parameters, replace Poisson's equation. Einstein also replaced the newtonian law of\n motion by the statement that free test particles move along geodesics, the shortest curves in the space-time\n geometry. The influential gravitation theorist John Archibald Wheeler has encapsulated general relativity in the\n aphorism ``curvature tells matter how to move, and matter tells space-time how to curve.'' The\n Eötvos-Dicke-Braginsky experiments demonstrate with high precision that free test particles all travel\n along the same trajectories in space-time, whereas the gravitational redshift shows (with more modest precision)\n these universal trajectories to be identical with geodesics.\n         Despite the great contrast between General Relativity and Newtonian theory, predictions of the former approach\n the latter for systems in which velocities are small compared to c and gravitational potentials are weak enough\n that they cannot cause larger velocities. This is why we can discuss with newtonian theory the structure of the\n earth and planets, stars and stellar clusters, and the gross features of motions in the solar system without fear\n of error.\n        Einstein noted two other predictions of General Relativity. First, light beams passing near a gravitating body must\n suffer a slight deflection proportional to that body's mass. First verified by observations of stellar images during\n the 1919 total solar eclipse, this effect also causes deflection of quasar radio images by the sun, is the likely cause\n of the phenomenon of ``double quasars'' with identical redshift and of the recently discovered giant arcs in clusters\n of galaxies (both probably effects of gravitational lensing), and is part and parcel of the black hole phenomenon.\n In a closely related effect first noted by Irwin Shapiro, radiation passing near a gravitating body is delayed in its\n flight in proportion to the body's mass, a time delay verified by means of radar waves deflected by the sun on their\n way from Earth to Mercury and back.\n        The second effect is the precession of the periastron of a binary system. According to newtonian gravitation, the\n orbit of each member of a binary is a coplanar ellipse with orientation fixed in space. General relativity predicts a\n slow rotation of the ellipse's major axis in the plane of the orbit (precession of the periastron). Originally verified\n in the motion of Mercury, the precession has of late also been detected in the orbits of binary pulsars.\n\nAll three effects mentioned depend on features of General Relativity beyond the weak equivalence principle. Indeed,\n Einstein built into general relativity the much more encompassing ``strong equivalence principle'': the local forms of\n all nongravitational physical laws and the numerical values of all dimensionless physical constants arc the same in the\n presence of a gravitational field as in its absence. In practice this implies that within any region in a gravitational field,\n sufficiently small that space-time curvature may be ignored, all physical laws, when expressed in terms of the space-time\n metric, have the same forms as required by special relativity in terms of the metric of Minkowski space-time. Thus in a\n small region in the neighborhood of a black hole (the source of a strong gravitational field) we would describe\n electromagnetism and optics with the same Maxwell equations used in earthly laboratories where the gravitational field\n is weak, and we would employ the laboratory values of the electrical permittivity and magnetic\n susceptibility of the vacuum.");
+        jScrollPane1.setViewportView(jTextArea1);
+
+        jLabel1.setIcon(new javax.swing.ImageIcon("/Users/danny/LIGOInterface/LIGO/images/Einstein gravity.jpg")); // NOI18N
+        jLabel1.setText("jLabel1");
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(154, 154, 154)
+                .addContainerGap()
                 .addComponent(jButton1)
-                .addContainerGap(191, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 1118, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(1, 1, 1)
+                .addComponent(jLabel1)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(120, 120, 120)
-                .addComponent(jButton1)
-                .addContainerGap(157, Short.MAX_VALUE))
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel1)
+                    .addComponent(jButton1)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 1581, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         pack();
@@ -106,5 +127,8 @@ public class Panel2 extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTextArea jTextArea1;
     // End of variables declaration//GEN-END:variables
 }
